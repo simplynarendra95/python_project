140c15489e6322e60056f0c6212cd8d56a880088
simplynarendra95 <44805615+simplynarendra95@users.noreply.github.com>
2024-11-28 09:22:24 +0530

    Update check_conn.py

--- Python_DB_By_GShow/check_conn.py
+++ Python_DB_By_GShow/check_conn.py
@@ -11,7 +11,7 @@ try:
     )
     if con.is_connected():
         print("Connection established....")
-        # sql = "create database geekyShow"
+        # sql = "create database testdb"
         # cur = con.cursor()
         # cur.execute(sql)
 except:
